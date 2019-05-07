536492887ac06427a20bf42f3c267171e8c624fa
Zurglots <bfeole@gmail.com>
2019-05-07 17:33:37 -0400

    callback assignment complete array methods wip

--- assignments/callbacks.js
+++ assignments/callbacks.js
@@ -1,6 +1,8 @@
-// Create a higher order function and invoke the callback function to test your work. You have been provided an example of a problem and a solution to see how this works with our items array.  Study both the problem and the solution to figure out the rest of the problems.
+// Create a higher order function and invoke the callback function to test your work.
+// You have been provided an example of a problem and a solution to see how this works with our items array.
+// Study both the problem and the solution to figure out the rest of the problems.
 
-const items = ['Pencil', 'Notebook', 'yo-yo', 'Gum'];
+const items = ["Pencil", "Notebook", "yo-yo", "Gum"];
 
 /* 
 
@@ -24,27 +26,64 @@ const items = ['Pencil', 'Notebook', 'yo-yo', 'Gum'];
 
 */
 
+// function getLength(arr, cb) {
+//   // getLength passes the length of the array into the callback.
+//   return cb(arr.length);
+// }
+// getLength(items, function(theLength) {
+//   console.log("getLength", theLength);
+// });
 
-function getLength(arr, cb) {
-  // getLength passes the length of the array into the callback.
-}
+// Arrow Attempt
+const getLength = (arr, cb) => cb(arr.length);
 
-function last(arr, cb) {
-  // last passes the last item of the array into the callback.
-}
+getLength(items, length => {
+  console.log("getLength", length);
+});
 
-function sumNums(x, y, cb) {
-  // sumNums adds two numbers (x, y) and passes the result to the callback.
-}
+// Multiple Line Arrow Example
+// const getLength = (arr, cb) => {
+//   arr.map(item => {
+//     console.log(item);
+//   });
+//   return;
+// };
 
-function multiplyNums(x, y, cb) {
-  // multiplyNums multiplies two numbers and passes the result to the callback.
-}
+// last passes the last item of the array into the callback.
+const last = (arr, cb) => cb(arr[arr.length - 1]);
 
-function contains(item, list, cb) {
-  // contains checks if an item is present inside of the given array/list.
-  // Pass true to the callback if it is, otherwise pass false.
-}
+last(items, theLast => {
+  console.log("theLast", theLast);
+});
+
+// sumNums adds two numbers (x, y) and passes the result to the callback.
+const sumNums = (x, y, cb) => cb(x + y);
+
+sumNums(1, 2, addMe => {
+  console.log("addMe", addMe);
+});
+
+// multiplyNums multiplies two numbers and passes the result to the callback.
+const multiplyNums = (x, y, cb) => cb(x * y);
+
+multiplyNums(2, 4, timesMe => {
+  console.log("timesMe", timesMe);
+});
+
+// contains checks if an item is present inside of the given array/list.
+// Pass true to the callback if it is, otherwise pass false.
+// const contains = (item, list, cb) => {
+//   list.map(
+//     if (list[i].includes(item)) {
+//       return true;
+//     } else {
+//       return false;
+//     });
+// };
+
+// contains("Gum", items, doesInclude => {
+//   console.log("doesInclude", doesInclude);
+// });
 
 /* STRETCH PROBLEM */
 
